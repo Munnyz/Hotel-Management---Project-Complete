2d26a679075822a8b9e2eb8986d5dd55affc7dca
Munnyz <65402631+Munnyz@users.noreply.github.com>
2022-02-11 19:34:51 -0300

    Add files via upload

---
+++ Documents/dbo.ContactViewOrSearch.sql
@@ -0,0 +1,6 @@
+﻿ALTER PROCEDURE dbo.ContactViewOrSearch
+@ContactName nvarchar(50)
+AS
+	SELECT *
+	FROM tab_contato
+	WHERE name LIKE @ContactName+'%'
---
+++ Documents/dbo.Table.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE tab_contato
+(
+	[ContactId] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] NVARCHAR(50) NULL, 
+    [Sobrenome] NVARCHAR(50) NULL
+)
